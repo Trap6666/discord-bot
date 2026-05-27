519c3f935f3d6e74f434b43ca1d7e204a361cc22
Aviv <vertman.aviv@gmail.com>
2026-05-27 22:10:14 +0300

    fix help command

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ intents.message_content = True
 intents.voice_states = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
